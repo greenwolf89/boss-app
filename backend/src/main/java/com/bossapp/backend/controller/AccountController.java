4358540c2476a2b491d3b7651da7520b9cb4e655
waylen_roche <waylenroche@gmail.com>
2023-01-18 14:30:14 -0500

    PostgreSQL + Spring Data JPA + Spring Boot initialized

---
+++ backend/src/main/java/com/bossapp/backend/controller/AccountController.java
@@ -0,0 +1,105 @@
+package com.bossapp.backend.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.bossapp.backend.model.Account;
+import com.bossapp.backend.repository.AccountRepository;
+
+@CrossOrigin(origins = "http://localhost:8081")
+@RestController
+@RequestMapping("/api")
+public class AccountController {
+
+    @Autowired
+    AccountRepository accountRepository;
+
+    @GetMapping("/accounts")
+    public ResponseEntity<List<Account>> getAllAccounts(@RequestParam(required=false) String username) {
+        try {
+            List<Account> accounts = new ArrayList<Account>();
+
+            if(username == null)
+                accountRepository.findAll().forEach(accounts::add);
+            else
+                accountRepository.findByUsername(username).forEach(accounts::add);
+
+            if(accounts.isEmpty()) {
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+            return new ResponseEntity<>(accounts, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+    @GetMapping("/accounts/{id}")
+    public ResponseEntity<Account> getAccountById(@PathVariable("id") long id){
+        Optional<Account> accountData = accountRepository.findById(id);
+
+        if(accountData.isPresent()) {
+            return new ResponseEntity<>(accountData.get(), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+    @PostMapping("/accounts")
+    public ResponseEntity<Account> createAccount(@RequestBody Account account){
+        try {
+			Account _account = accountRepository
+					.save(new Account(account.getId(), account.getUsername(), account.getEmail(), account.getPassword()));
+			return new ResponseEntity<>(_account, HttpStatus.CREATED);
+		} catch (Exception e) {
+			return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+    }
+    @PutMapping("/accounts/{id}")
+	public ResponseEntity<Account> updateTutorial(@PathVariable("id") long id, @RequestBody Account account) {
+		Optional<Account> accountData = accountRepository.findById(id);
+
+		if (accountData.isPresent()) {
+			Account _account = accountData.get();
+			_account.setId(account.getId());
+			_account.setUsername(account.getUsername());
+			_account.setEmail(account.getEmail());
+			return new ResponseEntity<>(accountRepository.save(_account), HttpStatus.OK);
+		} else {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+	}
+
+	@DeleteMapping("/accounts/{id}")
+	public ResponseEntity<HttpStatus> deleteTutorial(@PathVariable("id") long id) {
+		try {
+			accountRepository.deleteById(id);
+			return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+		} catch (Exception e) {
+			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+
+	@DeleteMapping("/accounts")
+	public ResponseEntity<HttpStatus> deleteAllaccounts() {
+		try {
+			accountRepository.deleteAll();
+			return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+		} catch (Exception e) {
+			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+
+	}
+}
--- backend2/src/main/resources/schema.sql
+++ backend2/src/main/resources/schema.sql
@@ -3,7 +3,6 @@ CREATE table accounts
 	id BIGINT NOT NULL,
 	username VARCHAR(255) NOT NULL,
 	email VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL,
+    password VARCHAR(65) NOT NULL,
 	PRIMARY KEY(id)
 );
-
