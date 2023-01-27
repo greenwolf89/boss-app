2fdfeb01defff159104b03900698f2e2a6c86b68
waylen_roche <waylenroche@gmail.com>
2023-01-27 12:46:57 -0500

    Working on setting up Proxy Between Frontend and Backend

--- frontend/src/features/transactions/transactionsAPI.ts
+++ frontend/src/features/transactions/transactionsAPI.ts
@@ -1,8 +1,12 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { EndpointBuilder } from "@reduxjs/toolkit/dist/query/endpointDefinitions";
+import { BaseQueryFn, createApi, EndpointDefinitions, FetchArgs, fetchBaseQuery, FetchBaseQueryError, FetchBaseQueryMeta } from "@reduxjs/toolkit/query/react";
 
-export const transactionsApi = createApi<TransactionType>({
+export const transactionsApi = createApi({
   reducerPath: "transactions",
-  baseQuery: fetchBaseQuery<TransactionType>({
+  baseQuery: fetchBaseQuery({
     baseUrl: process.env.REACT_APP_BOSS_API_HOST as string,
   }),
+  endpoints: function (build: EndpointBuilder<BaseQueryFn<string | FetchArgs, unknown, FetchBaseQueryError, {}, FetchBaseQueryMeta>, never, "transactions">): EndpointDefinitions {
+    throw new Error("Function not implemented.");
+  }
 });
